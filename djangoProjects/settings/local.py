1b27bcf2c9db5c5870004edc878256081d2c6dc8
esangayt <24_de_7@protonmail.com>
2024-09-07 21:28:55 -0500

    Configuración base

---
+++ djangoProjects/settings/local.py
@@ -0,0 +1,24 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5431',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# MEDIA_ROOT = BASE_DIR.child('media')
+
