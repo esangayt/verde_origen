5499d288506fc8ebb3805bb758ccd6f0c19c52a3
esangayt <24_de_7@protonmail.com>
2025-06-30 22:57:50 -0500

    feat: Modulo de cosechar

--- packages/farming/admin.py
+++ packages/farming/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from packages.farming.models import Harvest, Distribution
+
+
 # Register your models here.
+@admin.register(Harvest)
+class HarvestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tree', 'harvest_date', 'quantity',
+                    'measurement')
+
+@admin.register(Distribution)
+class DistributionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'harvest', 'distribution_date', 'quantity',
+                    'measurement', 'type')
+    list_filter = ('type','measurement')
--- packages/farming/models.py
+++ packages/farming/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+from packages.production.models import Tree
+
+
 # Create your models here.
+class Harvest(models.Model):
+    class KindQuantity(models.TextChoices):
+        KILOGRAMS = 'kg', 'Kilograms'
+        LITERS = 'l', 'Liters'
+        HUNDREDS = 'h', 'Hundreds'
+
+    tree = models.ForeignKey(Tree, on_delete=models.CASCADE)
+    harvest_date = models.DateField()
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    measurement = models.CharField(choices=KindQuantity.choices, max_length=2,
+                            default=KindQuantity.HUNDREDS)
+    quality = models.CharField(max_length=100, blank=True, null=True)
+    observations = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.tree.species.name} - {self.quantity} on {self.harvest_date}"
+
+class Distribution(models.Model):
+    # sometimes, not all harvest we will sale, same parts I will keep for myself
+    # or I will give to my family, or I will discard
+    class Type(models.TextChoices):
+        SALE = 'sale', 'Sale'
+        FAMILY = 'family', 'Family'
+        DISCARD = 'discard'
+
+    harvest = models.ForeignKey(Harvest, on_delete=models.CASCADE)
+    distribution_date = models.DateField()
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    measurement = models.CharField(choices=Harvest.KindQuantity.choices,
+                             max_length=2,
+                            default=Harvest.KindQuantity.HUNDREDS)
+    type = models.CharField(choices=Type.choices, max_length=10, default=Type.SALE)
+    observations = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.harvest.tree.species.name} - {self.quantity} {self.get_measurement_display()} distributed as {self.get_type_display()} on {self.distribution_date}"
